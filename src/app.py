27440a04c3d5fb336870fac8cfa38d806745fc5a
sdotchang <samuelzychang@gmail.com>
2024-07-29 14:46:12 -0700

    ADD /setup route to app.py for setup page CREATE setup.html template that includes form for studentID and allocation time CHANGE base route (index) to redirect to /setup route

--- src/app.py
+++ src/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('setup'))
+
+@app.route('/setup', methods=['GET','POST'])
+def setup():
+    if request.method == 'POST':
+        studentID = request.form.get('studentID')
+        allocationTime = request.form.get('allocationTime')
+        print(f'[SUBMISSION] StudentID: {studentID} Time: {allocationTime}')
+    return render_template('setup.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
