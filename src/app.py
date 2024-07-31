d759219b3e850961b580232b69041bf17fda3e63
sdotchang <samuelzychang@gmail.com>
2024-07-30 17:42:10 -0700

    Bug fix and Update Fix user being able to submit negative studentID and allocation time (studentID now minimum of 0 with steps of 1, allocation time minimum of 0.5 hours and steps of 0.5) Add credentials.html placeholder template Change '/setup' route from returning same page to redirecting to '/credentials' Add '/credentials' with method GET as placeholder

--- src/app.py
+++ src/app.py
@@ -9,10 +9,16 @@ def index():
 @app.route('/setup', methods=['GET','POST'])
 def setup():
     if request.method == 'POST':
+        ## Temporarily just grabbing data locally here, remove comment when SSH credentials is implemented
         studentID = request.form.get('studentID')
         allocationTime = request.form.get('allocationTime')
-        print(f'[SUBMISSION] StudentID: {studentID} Time: {allocationTime}')
+        print(f'[REQUEST] StudentID: {studentID} Time: {allocationTime} hours')
+        return redirect(url_for('credentials'))
     return render_template('setup.html')
+
+@app.route('/credentials', methods=['GET'])
+def credentials():
+    return render_template('credentials.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
